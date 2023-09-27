f7a7b2f25c70ac8ca663f4953b6ae52c3a303960
Baranimph <baranidharan.s@mphasis.com>
2023-09-27 16:34:01 +0530

    authentication code

--- Controllers/Login_controller.js
+++ Controllers/Login_controller.js
@@ -1,4 +1,8 @@
 const modelmanagername = require("../models/managername");
+const modelcandiate=require("../models/candidatelist");
+// const express=require('express');
+const bodyparser=require('body-parser');
+const jwt=require('jsonwebtoken');
 
 
 exports.signup = async (req, res) => {
@@ -43,12 +47,41 @@ exports.signup = async (req, res) => {
 
   }
 
+  const secretkey='';
+  async function verifyuser(username,password){
+    const candidate=await modelcandiate.findOne({username});
+    const manager=await modelmanagername.findOne({username});
+    if(candidate){
+      if(candidate.password===password){
+
+        return{role:'user',username:candidate.candidateName,permissions:candidate.permissions};
+
+      }
+    }
+    // if(mana)
+
+    if(manager){
+      if(manager.password===password){
+
+        return{role:'manager',username:manager.Managername,permissions:manager.permissions};
+
+      }
+    }
+
+    return null;
+  }
 exports.authenticate=async(req,res)=>{
   const{username,password}=req.body;
 
   const user=await verifyuser(username,password);
 
   if(user){
+    const token=jwt.sign({username:user.username,role:user.role},secretkey,{expiresIn:'1hr'});
+
+    res.json({success:true,message:'Aauthentication Successful',token});
     
+  }
+  else{
+    res.status(401).json({success:false,message:'Authentication Failed'});
   }
 }
--- models/candidatelist.js
+++ models/candidatelist.js
@@ -30,6 +30,14 @@ const candidatedetails = new mongoose.Schema({
   email_Filename: { type: String, required: true },
 
   questions: [questionSchema],
+  password:{
+    type:String,
+    required:true
+  },
+  confirmPassword:{
+    type:String,
+    required:true
+  }
 });
 
 module.exports = mongoose.model("candidatelist", candidatedetails);
